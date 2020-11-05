ddaab1efc0c66229cdeef70727b172ce642a1e5f
Your Name <helenemartin13@hotmail.com>
2020-11-05 19:37:06 +0000

    refactor exercise 2, test still don't pass

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -4,21 +4,11 @@ public class Exercise002 {
 
     public boolean isFromManchester(Person person) {
 
-        boolean fromManchester = false;
-
-        if (person.getCity().equals("Manchester")) {
-            fromManchester = true;
-        }
-        return fromManchester;
+        return person.getCity().toUpperCase().equals("MANCHESTER");
     }
 
     public boolean canWatchFilm(Person person, int ageLimit) {
-
-        boolean watchFilm = false;
-
-        if (person.getAge() >= ageLimit) {
-            watchFilm = true;
-        }
-        return watchFilm;
+        return person.getAge() >= ageLimit;
     }
+
 }
